8e0b4e3a725486e165abf3963c54e372d16cf083
GJ Tiquia <gjtiquia@gmail.com>
2026-05-20 11:31:05 +0800

    tmux-new draft

---
+++ tmux-new.sh
@@ -0,0 +1,8 @@
+# TODO : 
+# expectation:
+# - tmux-new <session-name>
+# - calls `tmux new -s <session-name>`
+# - automatically opens 3 windows
+# - first window typed "ff", enter, "v", no enter
+# - go to second window, type "lg", dont enter
+# - go back to first window and stay there
